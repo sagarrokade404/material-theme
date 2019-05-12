4f84ee5cde1372166e333703b92d68e2a72c2db9
sagar rokade <sagarrokade004@gmail.com>
2019-05-13 00:11:32 +0530

    sagar first

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { MaterialMainModule } from './material-main/material-main.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialMainModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
